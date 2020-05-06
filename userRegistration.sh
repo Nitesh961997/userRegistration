6176a052c460bd3d474118fb60fcab4a0de79672
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-06 14:21:07 +0530

    password input input can be taken

--- userRegistration.sh
+++ userRegistration.sh
@@ -28,7 +28,7 @@ for (( x=1; z<100; x++ ))
 z=0
 for (( x=1; z<100; x++ ))
 	do
-		emailPat="^[0-9 a-z A-Z]*[.-_]{0,3}[0-9 a-z A-Z]*[@]([a-z]*[.])+[a-z]{0,5}$"
+		emailPat="^[0-9 a-z A-Z]*[._-]{0,3}[0-9 a-z A-Z]*[@]([a-z]*[.])+[a-z]{0,5}$"
 		read -p "Enter your email Id=" emailId
 		if [[ $emailId =~ $emailPat ]]
 			then
@@ -44,6 +44,19 @@ for (( x=1; z<100; x++ ))
 		numPat="^[1-9]{1}[0-9]{9}"
 		read -p "Enter your mobiele no= 91" mobNum
 		if [[ $mobNum =~ $numPat ]]
+			then
+				echo valid
+				z=100
+			else
+				echo invalid
+		fi
+	done
+z=0
+for (( x=1; z<100; x++ ))
+	do
+		passwordPat="^([0-9a-zA-Z]*[.-@!#$%^&*]*){8}"
+		read -p "Enter a password= 91" password
+		if [[ $password =~ $passwordPat ]]
 			then
 				echo valid
 				z=100
